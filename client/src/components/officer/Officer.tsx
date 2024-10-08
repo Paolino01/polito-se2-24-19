a3e0749625b6a542f784d964dff6085004851d66
PAOLO CAGLIERO s324194 <s281559@studenti.polito.it>
2024-10-08 17:05:14 +0200

    Created Officer Home Page (super early version)

---
+++ client/src/components/officer/Officer.tsx
@@ -0,0 +1,15 @@
+function Officer() {
+    return (
+        <>
+            <h1>Counter n° 1</h1>
+            <span>Your services: </span> <span>Service 1, Service 3</span>
+
+            <h3>Customer: S-4</h3>
+
+            <input type="button" value="Served" />
+            <input type="button" value="Next" />
+        </>
+    );
+}
+
+export default Officer;
