5ab2315604b0f234bcc5f482fa1e9c388fbdaffa
PAOLO CAGLIERO s324194 <s281559@studenti.polito.it>
2024-10-09 15:58:33 +0200

    Added CSS to Officer GUI, Added Footer and Header, Started to work on data requests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Officer from './components/officer/Officer';
+import { BottomBar } from './components/bottombar/Bottombar';
+import { NavB } from './components/navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const markAsServed = async() => {
+
+  }
+
+  const nextCustomer = async() => {
+    
+  }
+
 
   return (
     <>
       <Routes>
         <Route path="/officer" element = {
-          <Officer />
+          <div>
+            <NavB />
+            <Officer markAsServed={markAsServed} nextCustomer={nextCustomer} />
+            <BottomBar />
+          </div>
         } />
       </Routes>
     </>
--- client/src/components/officer/Officer.tsx
+++ client/src/components/officer/Officer.tsx
@@ -1,15 +1,25 @@
-function Officer() {
+function Officer(props: any) {
     return (
-        <>
-            <h1>Counter n° 1</h1>
-            <span>Your services: </span> <span>Service 1, Service 3</span>
+        <div className="flex h-screen">
+            <div className="m-auto border-2 border-lightgray-500 rounded-xl p-3 size-5/6 bg-white flex flex-col justify-between">
+                <h1 className="text-6xl font-bold">Counter n° 1</h1>
 
-            <h3>Customer: S-4</h3>
+                <div className="pt-6 pb-6 text-center">
+                    <span className="text-4xl font-bold">Your services: </span> <span className="text-2xl">Service 1, Service 3</span>
+                    <br /><br />
 
-            <input type="button" value="Served" />
-            <input type="button" value="Next" />
-        </>
+                    <span className="text-4xl font-bold">Customer: </span> <span className="text-2xl">S-4</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <button type="button" className="border border-sky-500 rounded-lg p-1 text-xl hover:bg-sky-500 hover:text-white" onClick={() => props.markAsServed()}>Mark as Served</button>
+                    <button type="button" className="border border-sky-500 rounded-lg p-1 text-xl hover:bg-sky-500 hover:text-white" onClick={() => props.nextCustomer()}>Next Customer</button>
+                </div>
+            </div>
+        </div>
     );
 }
+
+
 
 export default Officer;
