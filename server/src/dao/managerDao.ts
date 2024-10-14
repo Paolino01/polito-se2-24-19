8e81e5c4a5430ce5dee6d2832c3c92d3346dc9d2
minasamadi <mina.samadi@gmail.com>
2024-10-14 23:12:11 +0200

    Implementing seeStats part

---
+++ server/src/dao/managerDao.ts
@@ -0,0 +1,47 @@
+import db from '../db'; // Import the DB connection
+
+export const getCustomerStatsByServiceType = (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT 
+                t.serviceid, 
+                s.name, 
+                COUNT(t.serviceid) as customer_count 
+            FROM ticket t
+            JOIN services s ON t.serviceid = s.serviceid
+            GROUP BY t.serviceid, s.name;
+        `;
+        
+        db.all(query, (err, rows) => {
+            if (err) {
+                reject(err); 
+            } else {
+                resolve(rows); 
+            }
+        });
+    });
+};
+
+// Get stats by counter
+export const getCustomerStatsByCounter = (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT 
+                t.counterid, 
+                t.serviceid, 
+                s.name, 
+                COUNT(t.serviceid) as customer_count 
+            FROM ticket t
+            JOIN services s ON t.serviceid = s.serviceid
+            GROUP BY t.counterid, t.serviceid;
+        `;
+        
+        db.all(query, (err, rows) => {
+            if (err) {
+                reject(err); 
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+};
