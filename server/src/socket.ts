cb82bb49cdac24c638ace437fb103bbd474f3968
Francesco <73170315+fra2404@users.noreply.github.com>
2024-10-16 17:56:50 +0200

    Refactor socket.ts to emit 'newCustomer' event with updated queue lengths

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,5 +1,7 @@
 import { Server } from 'socket.io';
 import http from 'http';
+import { emitEvent } from './services/socketService';
+import { getQueuesLengths } from './services/queueService';
 
 let io: Server;
 
@@ -11,8 +13,12 @@ export const initializeSocket = (server: http.Server): Server => {
         }
     });
 
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         console.log('Un client si è connesso');
+
+        const newQueueLengths = await getQueuesLengths();
+
+        emitEvent('newCustomer', newQueueLengths)
 
         socket.on('disconnect', () => {
             console.log('Un client si è disconnesso');
